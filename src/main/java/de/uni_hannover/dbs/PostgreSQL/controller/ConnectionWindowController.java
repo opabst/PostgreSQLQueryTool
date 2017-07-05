90a71c659850f91935416b162d483f24bd9c24e4
Oliver Pabst <oliverpabst.gm@googlemail.com>
2017-07-05 09:47:46 +0200

    added relevant identifiers of FXML-Objects from view

--- src/main/java/de/uni_hannover/dbs/PostgreSQL/controller/ConnectionWindowController.java
+++ src/main/java/de/uni_hannover/dbs/PostgreSQL/controller/ConnectionWindowController.java
@@ -1,7 +1,30 @@
 package de.uni_hannover.dbs.PostgreSQL.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 /**
  * Created by pabst on 05.07.17.
  */
 public class ConnectionWindowController {
+
+    @FXML
+    private TextField ConWinConnectionNameTF;
+
+    @FXML
+    private TextField ConWinHostnameTF;
+
+    @FXML
+    private TextField ConWinDatabaseTF;
+
+    @FXML
+    private TextField ConWinUsernameTF;
+
+    @FXML
+    private PasswordField ConWinPasswordTF;
+
+    public ConnectionWindowController() {
+
+    }
 }
