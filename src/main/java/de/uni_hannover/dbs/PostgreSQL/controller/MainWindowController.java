c189ecfe826fc33b8f18f895ae648b04d8ea959e
Oliver Pabst <oliver.pabst@outlook.de>
2017-07-10 07:30:27 +0200

    referenced FXML-objects in controller

--- src/main/java/de/uni_hannover/dbs/PostgreSQL/controller/MainWindowController.java
+++ src/main/java/de/uni_hannover/dbs/PostgreSQL/controller/MainWindowController.java
@@ -1,7 +1,28 @@
 package de.uni_hannover.dbs.PostgreSQL.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeView;
+
 /**
  * Created by pabst on 05.07.17.
  */
 public class MainWindowController {
+
+    @FXML
+    private TreeView MainWindowDBObjectOutlineTV;
+
+    @FXML
+    private TextArea MainWindowQueryTA;
+
+    @FXML
+    private TableView MainWindowResultTV;
+
+    @FXML
+    private TextArea MainWindowExplainPlanTA;
+
+    public MainWindowController() {
+
+    }
 }
