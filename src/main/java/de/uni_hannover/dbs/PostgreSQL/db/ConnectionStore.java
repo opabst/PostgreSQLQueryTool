439d54a56b9b43e762f46e220c43aa2f79b67c7a
Oliver Pabst <oliver.pabst@mailbox.org>
2019-07-24 11:10:25 +0200

    Implementierung eines (Singleton)-Verbindungsmanagers

---
+++ src/main/java/de/uni_hannover/dbs/PostgreSQL/db/ConnectionStore.java
@@ -0,0 +1,27 @@
+package de.uni_hannover.dbs.PostgreSQL.db;
+
+import de.uni_hannover.dbs.PostgreSQL.model.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class ConnectionStore {
+    private static ConnectionStore _instance;
+
+    private ObservableList<DBConnection> conList;
+
+    private ConnectionStore() {
+        conList = FXCollections.observableArrayList();
+    }
+
+    public static ConnectionStore getInstance() {
+        if(_instance == null) {
+            _instance = new ConnectionStore();
+        }
+
+        return _instance;
+    }
+
+    public void addConnection(DBConnection _con) {
+        conList.add(_con);
+    }
+}
