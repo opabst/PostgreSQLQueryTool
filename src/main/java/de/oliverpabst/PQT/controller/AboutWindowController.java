e080fc617e95de185af53d448eb21ab202ed2064
Oliver Pabst <opabst@icloud.com>
2019-11-18 10:25:23 +0100

    - HTMLEditor durch WebView ersetzt

--- src/main/java/de/oliverpabst/PQT/controller/AboutWindowController.java
+++ src/main/java/de/oliverpabst/PQT/controller/AboutWindowController.java
@@ -5,33 +5,31 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 
 public class AboutWindowController {
 
     @FXML
-    private HTMLEditor aboutTextHTML;
+    private WebView aboutWebView;
 
     @FXML
     public void initialize() {
         String htmlMessage =
-                "<html>" +
-                        "<head>PostgreSQL-Query-Tool</head>" +
-                        "<body>" +
-                        "<b>Autor</b>: Oliver Pabst &#9400; 2019<br>" +
-                        "<b>Lizenz</b>: <a href=&quot;https://www.gnu.org/licenses/gpl-3.0.en.html&quot;>GNU General Public License v3.0</a>" +
-                        "</body>" +
-                        "</html>";
+                "<p><h1><b>PostgreSQL-Query-Tool</b></h1></p>\n" +
+                "<br/>\n" +
+                "<p><b>Autor</b>: Oliver Pabst &#9400; 2019<br></p>\n" +
+                "<p><b>Lizenz</b>: <a href=&quot;https://www.gnu.org/licenses/gpl-3.0.en.html&quot;>GNU General Public License v3.0</a></p>\n";
 
-        aboutTextHTML.setHtmlText(htmlMessage);
+        WebEngine engine = aboutWebView.getEngine();
+        engine.loadContent(htmlMessage);
     }
 
     @FXML
     public void closeScreen(ActionEvent event) {
-        Scene aboutWindowScene = ((Button)event.getTarget()).getScene(); // TODO: Stage laden
-        Stage stage = new Stage();
-        stage.setScene(aboutWindowScene);
+        Stage stage = (Stage)((Button)event.getSource()).getScene().getWindow();
         stage.close();
     }
 }
